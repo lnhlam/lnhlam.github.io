66777176712de1c8990bf8bb83be6dc264f7f96e
lepotator <lenny@kippo.com>
2023-02-08 00:57:58 -0800

    fix

--- js/menulinks.js
+++
@@ -1,8 +0,0 @@
-const menuLinks = document.querySelectorAll('.menu a');
-
-menuLinks.forEach(link => {
-  const path = link.getAttribute('href');
-  if (window.location.pathname === path) {
-    link.classList.add('active');
-  }
-});
